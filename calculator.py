7ca5b1fbe178a4b766614778821663755becc58f
andy <andyrociogtz@gmail.com>
2016-09-02 21:34:06 -0500

    suma_numeros

--- calculator.py
+++ calculator.py
@@ -8,9 +8,14 @@ class CalculatorClass(object):
     Your class documentation here
     """
 
-    def sum(self, num_list):
+    def sum(num):
         """
         Your method documentation here
         """
+        sum=0								  #indica cantidad de numeros a sumar
+        num= int (input("ingresa un numero")) #solicita el ingreso de N numeros
+        for i in range(1,num+1):			  #rango  definido 
+        	sum=sum+i 						  #suma de N numeros definidos
+        print(sum)							  #resultado de la suma
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        

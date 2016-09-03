a5d392a59b59593828f35fad0b577c6b77c5ab77
andy <andyrociogtz@gmail.com>
2016-09-03 15:02:01 -0500

    tablas_employee_depto

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,37 +4,37 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
-						id_employee int, 
-						first_name varchar(50), 
-						last_name varchar(50), 
-						id_departament int,
-						id_rango int
+                        id serial,
+                        first_name varchar(50),
+                        last_name varchar(50),
+                        PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_department (
-						id_dept int,
-						name varchar(50), 
-						description varchar(50)
-);
-alter table employee add constraint fk_id_employee_departament 
-foreign key(id_dept) references employee_departament(id_dept) 
-on delete cascade on update cascade;
-
-CREATE TABLE employee_hobby (
-						id_hobby int, 
-						name varchar(40), 
-						description varchar(50)
+                        id serial,
+                        name varchar(50),
+                        description varchar(50),
+                        PRIMARY KEY (id)
 );
 
-CREATE TABLE hobby_employee (
-						id_employee int,
-						id_hobby int
-);
+ALTER TABLE employee
+  ADD COLUMN id_department integer;
+ALTER TABLE employee
+  ADD FOREIGN KEY (id_department) REFERENCES employee_department (id);
 
-CREATE TABLE range(
-						id_rango int, 
-						description varchar(40)
-);
-alter table employee add constraint fk_id_rango foreign key(id_rango) 
-references rango(id_rango) on delete cascade on update cascade;
--- ...
+INSERT INTO employee_department(name, description)
+    VALUES ('RH', 'Recursos Humanos'),
+           ('Ventas', 'Ventas'),
+           ('Administrativo', 'contable'),
+           ('TI', 'Infraestructuram'),
+           ('Sistemas', 'Desarrollo'),
+           ('Fabricacion', 'Manufacturas')
+;
+
+INSERT INTO employee(first_name, last_name, id_department)
+        VALUES ('Laura', 'Estrada', 1),
+                   ('Felipe', 'Millan', 5),
+                   ('Christian', 'Gutierrez', 4),
+                   ('Fabian', 'Vallejo', 1)
+;
+

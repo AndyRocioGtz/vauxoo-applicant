1e281902bdde103eb0ee15a61fe353ed0eba6948
andy <andyrociogtz@gmail.com>
2016-09-03 15:43:39 -0500

    correcion de errores flake8

--- calculator.py
+++ calculator.py
@@ -13,6 +13,6 @@ class CalculatorClass(object):
         Your method documentation here
         """
         suma = 0  # indica cantidad de numeros a sumar
-        for val in num: # rango  definido
+        for val in num:  # rango  definido
             suma = suma + val  # suma de N numeros definidos
         return suma
--- primes.py
+++ primes.py
@@ -15,7 +15,7 @@ class PrimeClass(object):
         if num < 2:  # si este numero es menor que 2 no es primo
             return False  # por lo tanto devolvera Falso
         for div in range(2, num):  # un rango desde el  dos hasta
-                                   # el numero que se define
+                                    # el numero que se define
             if num % div == 0:  # si el resto da 0 no es primo,
                 return False    # por lo que devolvera Falso
         return True  # de lo contrario regresara True

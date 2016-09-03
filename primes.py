1c67986ea9547f617cec6b05265001bdf3d67d6a
andy <andyrociogtz@gmail.com>
2016-09-02 21:33:40 -0500

    numero_primos

--- primes.py
+++ primes.py
@@ -8,9 +8,15 @@ class PrimeClass(object):
     Your class documentation here
     """
 
-    def is_prime(self, num_int):
+    def is_prime(num):
         """
         Your method documentation here
         """
+        if num < 2; 			#si este número es menor que 2 no es primo
+        	return False		#por lo tanto devolverá Falso
+        for i in range(2,num):	#un rango desde el  dos hasta el numero que se defina
+        	if num % i == 0:	#si el resto da 0 no es primo, por lo que devolerá Falso
+        		return False
+        return True				#de lo contrario regresará True
+    print is_prime(1)			#imprime resultado
         # your primes code here
-        return "not implement yet"  # Remove this dummy line

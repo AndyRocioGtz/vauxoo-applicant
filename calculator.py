ccae30165d1b011a9994e9585c81f9e1ae100e3b
andy <andyrociogtz@gmail.com>
2016-09-03 15:23:28 -0500

    suma_numeros

--- calculator.py
+++ calculator.py
@@ -8,14 +8,11 @@ class CalculatorClass(object):
     Your class documentation here
     """
 
-    def sum(num):
+    def sum(self, num):
         """
         Your method documentation here
         """
-        sum=0								  #indica cantidad de numeros a sumar
-        num= int (input("ingresa un numero")) #solicita el ingreso de N numeros
-        for i in range(1,num+1):			  #rango  definido 
-        	sum=sum+i 						  #suma de N numeros definidos
-        print(sum)							  #resultado de la suma
-        # your sum code here
-        
+        suma = 0  # indica cantidad de numeros a sumar
+        for val in num: # rango  definido
+            suma = suma + val  # suma de N numeros definidos
+        return suma
